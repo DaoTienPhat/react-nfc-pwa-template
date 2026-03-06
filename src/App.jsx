7a540ce4c7ddcef9249f6d84754fe1c8aef63224
TOH81HC <Phat.DaoTien@vn.bosch.com>
2026-03-06 16:01:41 +0700

    first

---
+++ src/App.jsx
@@ -0,0 +1,74 @@
+
+import { useRef, useState } from "react"
+
+export default function App() {
+  const [img,setImg] = useState("/assets/default.jpg")
+  const [enabled,setEnabled] = useState(false)
+
+  const audioRef = useRef(null)
+  const wakeLock = useRef(null)
+
+  const tagMap = {
+    "04:A3:91:12:AA": {
+      img: "/assets/apple.png",
+      audio: "/audio/apple.mp3"
+    },
+    "04:77:22:88:BB": {
+      img: "/assets/banana.png",
+      audio: "/audio/banana.mp3"
+    }
+  }
+
+  async function startKiosk(){
+
+    try{
+
+      if("wakeLock" in navigator){
+        wakeLock.current = await navigator.wakeLock.request("screen")
+      }
+
+      const reader = new NDEFReader()
+      await reader.scan()
+
+      reader.onreading = (event)=>{
+
+        const uid = event.serialNumber
+        const asset = tagMap[uid]
+
+        if(!asset) return
+
+        setImg(asset.img)
+
+        audioRef.current.src = asset.audio
+        audioRef.current.play()
+
+      }
+
+      setEnabled(true)
+
+    }catch(e){
+      alert("NFC not supported or permission denied")
+      console.error(e)
+    }
+
+  }
+
+  return (
+    <div className="app">
+
+      {!enabled && (
+        <button className="start" onClick={startKiosk}>
+          Enable NFC Kiosk Mode
+        </button>
+      )}
+	  <link rel="preload" href="/assets/apple.png" as="image" />
+	  <link rel="preload" href="/assets/banana.png" as="image" />
+
+      <img src={img} className="display"/>
+
+      <audio ref={audioRef}/>
+
+    </div>
+  )
+
+}
