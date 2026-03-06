472eeac4371ef8db5c39a56566facc2b1c5420d7
TOH81HC <Phat.DaoTien@vn.bosch.com>
2026-03-06 16:41:16 +0700

    add icon

--- vite.config.js
+++ vite.config.js
@@ -17,12 +17,12 @@ export default defineConfig({
         theme_color: '#000000',
         icons: [
           {
-            src: '/icon-192.png',
+            src: '/public/assets/icon-192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: '/icon-512.png',
+            src: '/public/assets/icon-512.png',
             sizes: '512x512',
             type: 'image/png'
           }
