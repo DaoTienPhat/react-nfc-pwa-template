de6d2295ae76105e9729797852aa7f929b3199d4
PhatDao12301 <phat4everduke@gmail.com>
2026-03-06 23:25:04 +0700

    add nfc card of dark magician and better log

---
+++ src/tagMap.jsx
@@ -0,0 +1,24 @@
+const tagMap = {
+	"04:A3:91:12:AA": {
+		img: "/assets/apple.png",
+		audio: "/audio/apple.mp3"
+	},
+	"04:77:22:88:BB": {
+		img: "/assets/banana.png",
+		audio: "/audio/banana.mp3"
+	},
+	"1D:17:B0:D8:0A:10:80": {
+		title: "Akatsuki",
+		img: "/assets/akatsuki.png",
+		audio: "/audio/yugioh-001.mp3",
+		video: "/video/placeholder.mp4"
+	},
+	"D4:10:EE:1E:19:61:80": {
+		title: "Dark Magician",
+		img: "/assets/apple.png",
+		audio: "/audio/yugioh-001.mp3",
+		video: "/video/blue-eye.mp4"
+	}
+}
+
+export { tagMap }
